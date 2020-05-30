bde270d083412a7c2838e42125046b24e50f7bdf
Michel Romero Rodríguez <michelcenrr@gmail.com>
2020-05-30 05:02:52 -0300

    added collision detection

--- html/js/svg.js
+++ html/js/svg.js
@@ -2,6 +2,31 @@ function makeDraggable(evt){
   var svg =  evt.target;
   var selectedElement = false;
   var offset = [null, null, null];
+  const particleRadius = 10;
+  const l1 = {
+    x1: 40,
+    x2: 60,
+    y1: 0,
+    y2: 100
+  };
+  const l2 = {
+    x1: 60,
+    x2: 40,
+    y1: 0,
+    y2: 100
+  };
+
+  svg.querySelector('#l1').setAttributeNS(null, 'x1', l1.x1);
+  svg.querySelector('#l1').setAttributeNS(null, 'x2', l1.x2);
+  svg.querySelector('#l1').setAttributeNS(null, 'y1', l1.y1);
+  svg.querySelector('#l1').setAttributeNS(null, 'y2', l1.y2);  
+  
+  svg.querySelector('#l2').setAttributeNS(null, 'x1', l2.x1);  
+  svg.querySelector('#l2').setAttributeNS(null, 'x2', l2.x2);  
+  svg.querySelector('#l2').setAttributeNS(null, 'y1', l2.y1);  
+  svg.querySelector('#l2').setAttributeNS(null, 'y2', l2.y2);  
+  
+  svg.querySelector('#circle').setAttributeNS(null, 'r', particleRadius);
 
   svg.addEventListener('mousedown', startDrag);
   svg.addEventListener('mousemove', drag);
@@ -23,6 +48,9 @@ function makeDraggable(evt){
       var r = getMousePosition(evt);
       selectedElement.setAttributeNS(null, "cx", r.x - offset.x);
       selectedElement.setAttributeNS(null, "cy", r.y - offset.y);
+
+
+      console.log(intersectionLineCircle(parseFloat(selectedElement.getAttributeNS(null, "cx")), parseFloat(selectedElement.getAttributeNS(null, "cy")), particleRadius, l1.x1, l1.y1, l1.x2, l1.y2));
     }
   }
 
@@ -39,6 +67,34 @@ function makeDraggable(evt){
   }
 
 
+  // Intersection between a line and a circle
+  // https://mathworld.wolfram.com/Circle-LineIntersection.html
+
+  function intersectionLineCircle(cx, cy, r, x1, y1, x2, y2){
+
+    // console.log([cx, cy, r, x1, y1, x2, y2]);
+    x1 -= cx; x2 -= cx;
+    y1 -= cy; y2 -= cy;
+    // console.log([cx, cy, r, x1, y1, x2, y2]);
+
+    var dx = x2 - x1;
+    var dy = y2 - y1;
+    var dr = Math.sqrt(dx*dx + dy*dy);
+    var D = x1*y2 - x2*y1;
+
+    var Delta = r*r *dr*dr - D*D;
+
+    if(Delta > 0) return [{
+      x: cx + (D*dy - Math.sign(dy)*dx*Math.sqrt(Delta))/(dr*dr),
+      y: cy + (-D*dx - Math.abs(dy)*Math.sqrt(Delta))/(dr*dr)
+    },
+    {
+      x: cx + (D*dy + Math.sign(dy)*dx*Math.sqrt(Delta))/(dr*dr),
+      y: cy + (-D*dx + Math.abs(dy)*Math.sqrt(Delta))/(dr*dr)
+    }];
+    
+    return null;
+  }
 
 
 
