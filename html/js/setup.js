83fcf905ecdca247363c94f04e1548c9ca3ed226
Michel Romero Rodríguez <michelcenrr@gmail.com>
2020-05-31 21:29:16 -0300

    muestra to setup

--- html/js/setup.js
+++ html/js/setup.js
@@ -1,4 +1,5 @@
 var setup = document.querySelector('#setup');
+var muestra = document.querySelector('#muestra');
 
 
 const ids = ["setup-laserfrombeamsplitter", 'setup-laser', 'setup-camera', 'setup-mirror2', 'setup-mirror1', 'setup-lens2', 'setup-lens1', 'setup-image2camera', 'setup-filter','setup-beamsplitter', 'setup-laser3beamsplitter', 'setup-laser1', 'setup-laser2filter',  'setup-laser2lens','setup-laser2mirror',
@@ -7,7 +8,19 @@ const ids = ["setup-laserfrombeamsplitter", 'setup-laser', 'setup-camera', 'setu
 const descriptions = ["50% &nbsp;&nbsp; A=4", `&lambda; = 633 nm &nbsp;&nbsp;  I = 17 mW`, 'Cámara de video', 'Espejo ajustable', 'Espejo ajustable', 'f = 15 cm', 'f = 5cm', 'Imagen a la cámara', 'Filtro','Divisor de haz', '100% &nbsp;&nbsp; A=4', '100% &nbsp;&nbsp; A=1', '25% &nbsp;&nbsp; A=4',  '100% &nbsp;&nbsp; A=1','100% &nbsp;&nbsp; A=4',
 'Ensanchamiento del haz', 'Espejo ajustable', '50% &nbsp;&nbsp; A=4', 'Posicionador XYZ', 'Fuente de luz blanca', 'Muestra', 'Objetivo de microscopio'];
 
+const muestraIds = ['portaobjeto', 'cubreobjeto', 'gota', 'parafilm'];
+
+const muestraDescriptions = ['Portaobjeto', 'Cubreobjeto', 'Muestra', 'Lámina espaciadora'];
+
 ids.forEach((id, i) => {
+  addTooltip(id, i, setup, descriptions);
+});
+
+muestraIds.forEach((id, i) => {
+  addTooltip(id, i, muestra, muestraDescriptions);
+});
+
+function addTooltip(id, i, setup, descriptions){
   var elm = setup.querySelector('#'+id);
 
   elm.addEventListener('mouseover', function(evt){
@@ -23,11 +36,7 @@ ids.forEach((id, i) => {
   elm.addEventListener('mouseleave', function(evt){
     tooltip.style.display = 'none';
   });
-
-  // console.log(elm);
-
-});
-
+}
 
 var tooltip = document.querySelector('#setup-tooltip');
 tooltip.style.position = 'absolute';
@@ -37,4 +46,23 @@ dy = -40;
 function showTooltip(evt){
   tooltip.style.left = (evt.pageX + dx) + "px";
   tooltip.style.top = (evt.pageY + dy) + "px";
+}
+
+var elm = setup.querySelector('#setup-muestra');
+var muestraContainer = document.querySelector('#muestra-container');
+
+  elm.addEventListener('mousedown', function(evt){
+    muestraContainer.style.display = 'block';
+  });
+
+  elm.addEventListener('mousemove', function(evt){
+    showTooltip(evt);
+  });
+  
+  elm.addEventListener('mouseleave', function(evt){
+    tooltip.style.display = 'none';
+  });
+
+function closeMuestra(){
+  muestraContainer.style.display = 'none';
 }
