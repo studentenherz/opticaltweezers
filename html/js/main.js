5a3f2199e473ce51ac7f9c256eb52efd8c66f7ec
Michel Romero Rodríguez <michelcenrr@gmail.com>
2020-06-06 21:14:49 -0300

    prettier

--- html/js/main.js
+++ html/js/main.js
@@ -1,92 +1,93 @@
-const slides = document.querySelectorAll('.slide');
+const slides = document.querySelectorAll(".slide");
 
-const prevBtn = document.querySelector('#prev');
-const nextBtn = document.querySelector('#next');
-const slidesNav = document.querySelector('.slidesNav');
+const prevBtn = document.querySelector("#prev");
+const nextBtn = document.querySelector("#next");
+const slidesNav = document.querySelector(".slidesNav");
 
 let index = -1; // current slide index
 
 slides.forEach((slide, i) => {
-  var node = document.createElement('span');
-  var tooltipNode = document.createElement('div');
-  tooltipNode.classList.add('tooltip');
-  var tooltipText = document.createTextNode(slide.querySelector('.short-title').innerHTML);
+  var node = document.createElement("span");
+  var tooltipNode = document.createElement("div");
+  tooltipNode.classList.add("tooltip");
+  var tooltipText = document.createTextNode(
+    slide.querySelector(".short-title").innerHTML
+  );
   tooltipNode.appendChild(tooltipText);
   node.appendChild(tooltipNode);
   slidesNav.appendChild(node);
 });
 
-window.setActiveSlide = function(x){
+window.setActiveSlide = function (x) {
   index = x;
   slides.forEach((slide, i) => {
-    if(i == index){
-      toggleShow(i, '.title');
-      if(window.chrome != undefined)
-        slide.scrollIntoView();
-      else
-        slide.scrollIntoView({ behavior: 'smooth'});
+    if (i == index) {
+      toggleShow(i, ".title");
+      if (window.chrome != undefined) slide.scrollIntoView();
+      else slide.scrollIntoView({ behavior: "smooth" });
     }
   });
-  
-  slidesNav.querySelectorAll('span').forEach((dot, j) => {
-    if(j == index) dot.classList.add('active');
-    else dot.classList.remove('active');
+
+  slidesNav.querySelectorAll("span").forEach((dot, j) => {
+    if (j == index) dot.classList.add("active");
+    else dot.classList.remove("active");
   });
-}
+};
 
-window.getDescription = function(){
-  if(slides[index].querySelector('.description'))
-    return slides[index].querySelector('.description').innerHTML;
+window.getDescription = function () {
+  if (slides[index].querySelector(".description"))
+    return slides[index].querySelector(".description").innerHTML;
   return null;
-}
+};
 
-slidesNav.querySelectorAll('span').forEach((dot, i) => {
-  dot.addEventListener('click', () => {setActiveSlide(i)});
+slidesNav.querySelectorAll("span").forEach((dot, i) => {
+  dot.addEventListener("click", () => {
+    setActiveSlide(i);
+  });
 });
 
 const toggleShow = (curr, elmClass) => {
   slides.forEach((slide, i) => {
     // console.log(slide);
-    if(curr == i) slide.querySelector(elmClass).classList.add('show');
-    else slide.querySelector(elmClass).classList.remove('show');
+    if (curr == i) slide.querySelector(elmClass).classList.add("show");
+    else slide.querySelector(elmClass).classList.remove("show");
   });
 };
 
-nextBtn.addEventListener('click', () => {
+nextBtn.addEventListener("click", () => {
   index = (index + slides.length + 1) % slides.length;
   setActiveSlide(index);
 });
 
-prevBtn.addEventListener('click', () => {
+prevBtn.addEventListener("click", () => {
   index = (index + slides.length - 1) % slides.length;
-  setActiveSlide(index); 
+  setActiveSlide(index);
 });
 
 const deltaScroll = 500;
 let lastTime = 0;
 
-window.addEventListener('wheel', (e) => {
+window.addEventListener("wheel", (e) => {
   const delta = e.deltaY;
   const currTime = new Date().getTime();
-  if(currTime - lastTime < deltaScroll){
+  if (currTime - lastTime < deltaScroll) {
     e.preventDefault;
     return;
   }
 
-  if(delta > 0){
-    const clickEvent = new Event('click');
+  if (delta > 0) {
+    const clickEvent = new Event("click");
     nextBtn.dispatchEvent(clickEvent);
-  }
-  else if(delta < 0){
-    const clickEvent = new Event('click');
+  } else if (delta < 0) {
+    const clickEvent = new Event("click");
     prevBtn.dispatchEvent(clickEvent);
   }
   lastTime = currTime;
 });
 
-const init = ()=>{
-  const clickEvent = new Event('click');
+const init = () => {
+  const clickEvent = new Event("click");
   nextBtn.dispatchEvent(clickEvent);
-}
+};
 
 init();
--- js/main.js
+++ js/main.js
@@ -1,37 +1,37 @@
-var tag = document.createElement('script');
-const presentation = document.getElementById('presentation');
-var description = document.querySelector('#description');
+var tag = document.createElement("script");
+const presentation = document.getElementById("presentation");
+var description = document.querySelector("#description");
 
 tag.src = "https://www.youtube.com/iframe_api";
-var firstScriptTag = document.getElementsByTagName('script')[0];
+var firstScriptTag = document.getElementsByTagName("script")[0];
 firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
 
 var player;
 function onYouTubeIframeAPIReady() {
-  player = new YT.Player('player', {
-    videoId: '97-y-AzuC8I',
-    playerVars: { 'rel':0}
+  player = new YT.Player("player", {
+    videoId: "97-y-AzuC8I",
+    playerVars: { rel: 0 },
   });
 
   setInterval(scrollIntoViewWithVideo, 500);
 }
 
 const timeMarks = {
-  13 : 1,
-  41 : 2,
+  13: 1,
+  41: 2,
   123: 3,
   238: 4,
   253: 5,
   309: 6,
   376: 7,
   403: 8,
-  458: 0
-}
+  458: 0,
+};
 
-function scrollIntoViewWithVideo () {
+function scrollIntoViewWithVideo() {
   var currTime = parseInt(player.getCurrentTime());
   // console.log(currTime);
-  if(timeMarks[currTime]){
+  if (timeMarks[currTime]) {
     presentation.contentWindow.setActiveSlide(timeMarks[currTime]);
   }
 }
